9ad424a028c36723c917e9790ebca1b925c04c53
Jean-Francois Roy <jf@devklog.net>
2020-01-20 17:17:23 -0800

    Switch to vex

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,77 @@
 #![warn(clippy::all)]
 
 extern crate image;
-extern crate nalgebra as na;
-extern crate nalgebra_glm as glm;
 extern crate palette;
 extern crate rand;
 extern crate rayon;
 
-use palette::{LinSrgb, Pixel, Srgb};
-use rand::distributions::Uniform;
 use rand::prelude::*;
 use rayon::prelude::*;
 use std::cell::RefCell;
 use std::option::Option;
+use vek::Lerp;
+
+type Vec3f = vek::vec::Vec3<f32>;
 
 // RNG
 
 thread_local!(static RNG: RefCell<SmallRng> = RefCell::new(SmallRng::from_entropy()));
 
+// vek::Vec traits
+
+trait RngVector {
+  /// Makes a new random vector using `rng`.
+  ///
+  /// Each component will be in the half-open range `[0,1)` (see
+  /// https://rust-num.github.io/num/rand/trait.Rng.html#method.next_f32).
+  /// 
+  /// This function does *not* create unit vectors.
+  fn with_rng<R: Rng + ?Sized>(rng: &mut R) -> Self;
+
+  /// Generates a random vector inside the unit sphere.
+  fn in_unit_sphere(rng: &mut impl Rng) -> Self;
+
+  /// Generates a random vector inside the unit disc in the XY plane. The Z
+  /// component shall be 0.
+  fn in_unit_disc(rng: &mut impl Rng) -> Self;
+}
+
+impl RngVector for Vec3f {
+  #[inline]
+  fn with_rng<R: Rng + ?Sized>(rng: &mut R) -> Vec3f {
+    Vec3f::new(rng.gen(), rng.gen(), rng.gen())
+  }
+
+  #[inline]
+  fn in_unit_sphere(rng: &mut impl Rng) -> Vec3f {
+    loop {
+      let v = 2. * Vec3f::with_rng(rng) - Vec3f::broadcast(1.);
+      if v.dot(v) < 1. {
+        return v;
+      }
+    }
+  }
+
+  #[inline]
+  fn in_unit_disc(rng: &mut impl Rng) -> Vec3f {
+    loop {
+      let v = 2. * Vec3f::new(rng.gen(), rng.gen(), 0.) - Vec3f::new(1., 1., 0.);
+      if v.dot(v) < 1. {
+        return v;
+      }
+    }
+  }
+}
+
 // Ray
 
 struct Ray {
-  origin: glm::Vec3,
-  direction: glm::Vec3,
+  origin: Vec3f,
+  direction: Vec3f,
 }
 
 impl Ray {
-  fn point_at_parameter(&self, t: f32) -> glm::Vec3 {
+  fn point_at_parameter(&self, t: f32) -> Vec3f {
     self.origin + (t * self.direction)
   }
 }
@@ -35,7 +80,7 @@ impl Ray {
 
 struct Scattered {
   r: Ray,
-  attenuation: glm::Vec3,
+  attenuation: Vec3f,
 }
 
 trait Material {
@@ -46,8 +91,8 @@ trait Material {
 
 struct Hit<'obj> {
   t: f32,
-  p: glm::Vec3,
-  normal: glm::Vec3,
+  p: Vec3f,
+  normal: Vec3f,
   material: &'obj (dyn Material + Sync),
 }
 
@@ -58,7 +103,7 @@ trait Hittable<'obj> {
 // Sphere
 
 struct Sphere<'obj> {
-  center: glm::Vec3,
+  center: Vec3f,
   radius: f32,
   material: &'obj (dyn Material + Sync),
 }
@@ -66,9 +111,9 @@ struct Sphere<'obj> {
 impl<'obj> Hittable<'obj> for Sphere<'obj> {
   fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<Hit> {
     let oc = r.origin - self.center;
-    let a = glm::dot(&r.direction, &r.direction);
-    let b = glm::dot(&oc, &r.direction);
-    let c = glm::dot(&oc, &oc) - (self.radius * self.radius);
+    let a = r.direction.dot(r.direction);
+    let b = oc.dot(r.direction);
+    let c = oc.dot(oc) - (self.radius * self.radius);
     let discriminant = (b * b) - (a * c);
     if discriminant < 0. {
       return None;
@@ -122,12 +167,13 @@ impl<'obj> Hittable<'obj> for Scene<'obj> {
 // Lambertian
 
 struct Lambertian {
-  albedo: glm::Vec3,
+  albedo: Vec3f,
 }
 
 impl Material for Lambertian {
   fn scatter(&self, _: &Ray, hit: &Hit) -> Option<Scattered> {
-    let target = hit.p + hit.normal + random_in_unit_sphere();
+    let target =
+      hit.p + hit.normal + RNG.with(|rng_rc| Vec3f::in_unit_sphere(&mut *rng_rc.borrow_mut()));
     let origin = hit.p;
     let direction = target - hit.p;
     let r = Ray { origin, direction };
@@ -139,18 +185,18 @@ impl Material for Lambertian {
 // Metal
 
 struct Metal {
-  albedo: glm::Vec3,
+  albedo: Vec3f,
   fuzz: f32,
 }
 
 impl Material for Metal {
   fn scatter(&self, r: &Ray, hit: &Hit) -> Option<Scattered> {
     let origin = hit.p;
-    let direction =
-      glm::reflect_vec(&r.direction.normalize(), &hit.normal) + self.fuzz * random_in_unit_sphere();
+    let direction = r.direction.normalized().reflected(hit.normal)
+      + self.fuzz * RNG.with(|rng_rc| Vec3f::in_unit_sphere(&mut *rng_rc.borrow_mut()));
     let r = Ray { origin, direction };
     let attenuation = self.albedo;
-    if glm::dot(&direction, &hit.normal) > 0. {
+    if direction.dot(hit.normal) > 0. {
       return Some(Scattered { r, attenuation });
     }
     None
@@ -170,31 +216,29 @@ struct Dielectric {
 
 impl Material for Dielectric {
   fn scatter(&self, r: &Ray, hit: &Hit) -> Option<Scattered> {
-    let unit_direction = r.direction.normalize();
-    let reflected = glm::reflect_vec(&unit_direction, &hit.normal);
-    let dir_dot_normal = glm::dot(&r.direction, &hit.normal);
+    let unit_direction = r.direction.normalized();
+    let reflected = unit_direction.reflected(hit.normal);
+    let dir_dot_normal = r.direction.dot(hit.normal);
     let (outward_normal, ni_over_nt, cosine) = if dir_dot_normal > 0. {
       (
         -hit.normal,
         self.ref_idx,
-        self.ref_idx * dir_dot_normal / glm::length(&r.direction),
+        self.ref_idx * dir_dot_normal / r.direction.magnitude(),
       )
     } else {
       (
         hit.normal,
         1. / self.ref_idx,
-        -dir_dot_normal / glm::length(&r.direction),
+        -dir_dot_normal / r.direction.magnitude(),
       )
     };
-    let refracted = glm::refract_vec(&unit_direction, &outward_normal, ni_over_nt);
-    let reflect_prob = if refracted != glm::Vec3::zeros() {
+    let refracted = unit_direction.refracted(outward_normal, ni_over_nt);
+    let reflect_prob = if refracted != Vec3f::zero() {
       schlick(cosine, self.ref_idx)
     } else {
       1.
     };
-    let r = if RNG.with(|rng_rc| Uniform::from(0f32..1f32).sample(&mut *rng_rc.borrow_mut()))
-      < reflect_prob
-    {
+    let r = if RNG.with(|rng_rc| rng_rc.borrow_mut().gen::<f32>()) < reflect_prob {
       Ray {
         origin: hit.p,
         direction: reflected,
@@ -205,7 +249,7 @@ impl Material for Dielectric {
         direction: refracted,
       }
     };
-    let attenuation = glm::vec3(1., 1., 1.);
+    let attenuation = Vec3f::broadcast(1.);
     Some(Scattered { r, attenuation })
   }
 }
@@ -213,18 +257,17 @@ impl Material for Dielectric {
 // Camera
 
 struct Camera {
-  lower_left_corner: glm::Vec3,
-  horizontal: glm::Vec3,
-  vertical: glm::Vec3,
-  origin: glm::Vec3,
+  lower_left_corner: Vec3f,
+  horizontal: Vec3f,
+  vertical: Vec3f,
+  origin: Vec3f,
 }
 
 impl Camera {
-  fn gen_ray(&self, uv: glm::Vec2) -> Ray {
+  fn gen_ray(&self, s: f32, t: f32) -> Ray {
     Ray {
       origin: self.origin,
-      direction: self.lower_left_corner + (uv.x * self.horizontal) + (uv.y * self.vertical)
-        - self.origin,
+      direction: self.lower_left_corner + (s * self.horizontal) + (t * self.vertical) - self.origin,
     }
   }
 }
@@ -232,124 +275,102 @@ impl Camera {
 // main
 
 // Traces a ray. This is `color` in the book.
-fn trace(r: &Ray, scene: &Scene, depth: i32) -> glm::Vec3 {
+fn trace(r: &Ray, scene: &Scene, depth: i32) -> Vec3f {
   if let Some(hit) = scene.hit(r, 0.001, std::f32::MAX) {
     if depth >= 50 {
-      return glm::Vec3::zeros();
+      return Vec3f::zero();
     }
     if let Some(sc) = hit.material.scatter(r, &hit) {
-      return glm::matrix_comp_mult(&sc.attenuation, &trace(&sc.r, scene, depth + 1));
+      return sc.attenuation * trace(&sc.r, scene, depth + 1);
     }
-    return glm::Vec3::zeros();
+    return Vec3f::zero();
   }
-  let white: glm::Vec3 = glm::Vec3::repeat(1.);
-  let sky_blue: glm::Vec3 = glm::vec3(0.5, 0.7, 1.0);
-  let unit_direction = r.direction.normalize();
+  const WHITE: Vec3f = Vec3f::new(1., 1., 1.);
+  const SKY_BLUE: Vec3f = Vec3f::new(0.5, 0.7, 1.0);
+  let unit_direction = r.direction.normalized();
   let t = 0.5 * (unit_direction.y + 1.);
-  glm::lerp(&white, &sky_blue, t)
-}
-
-#[inline(always)]
-fn random_in_unit_sphere() -> glm::Vec3 {
-  let d = Uniform::from(0f32..1f32);
-  loop {
-    let v = RNG.with(|rng_rc| glm::Vec3::from_distribution(&d, &mut *rng_rc.borrow_mut()));
-    let p = 2. * v - glm::Vec3::repeat(1.);
-    if glm::dot(&p, &p) < 1.0 {
-      return p;
-    }
-  }
-
-  // NOTE: This produces a much smaller shadow for the small sphere and other
-  // differences from the reference implementation that generates vectors inside
-  // the unit sphere (as opposed to on the surface).
-  // let sphere = rand::distributions::UnitSphereSurface::new();
-  // let s = RNG.with(|rng_rc| sphere.sample(&mut *rng_rc.borrow_mut()));
-  // glm::vec3(s[0] as f32, s[1] as f32, s[2] as f32)
+  Lerp::lerp(WHITE, SKY_BLUE, t)
 }
 
 fn main() {
-  let dim = glm::vec2(1000, 500);
-  let ns = 100;
+  const NX: usize = 1000;
+  const NY: usize = 500;
+  const NS: usize = 100;
+
+  eprintln!("Rendering {} x {} image using {} samples.", NX, NY, NS);
 
   let mut scene: Scene = Scene {
     spheres: Vec::new(),
   };
 
   let mat_lamb1 = Lambertian {
-    albedo: glm::vec3(0.1, 0.2, 0.5),
+    albedo: Vec3f::new(0.1, 0.2, 0.5),
   };
   let mat_lamb2 = Lambertian {
-    albedo: glm::vec3(0.8, 0.8, 0.),
+    albedo: Vec3f::new(0.8, 0.8, 0.),
   };
   let mat_metal1 = Metal {
-    albedo: glm::vec3(0.8, 0.6, 0.2),
+    albedo: Vec3f::new(0.8, 0.6, 0.2),
     fuzz: 0.3,
   };
   let mat_dia1 = Dielectric { ref_idx: 1.5 };
 
   scene.spheres.push(Sphere {
-    center: glm::vec3(0., 0., -1.),
+    center: Vec3f::new(0., 0., -1.),
     radius: 0.5,
     material: &mat_lamb1,
   });
   scene.spheres.push(Sphere {
-    center: glm::vec3(0., -100.5, -1.),
+    center: Vec3f::new(0., -100.5, -1.),
     radius: 100.,
     material: &mat_lamb2,
   });
   scene.spheres.push(Sphere {
-    center: glm::vec3(1., 0., -1.),
+    center: Vec3f::new(1., 0., -1.),
     radius: 0.5,
     material: &mat_metal1,
   });
   scene.spheres.push(Sphere {
-    center: glm::vec3(-1., 0., -1.),
+    center: Vec3f::new(-1., 0., -1.),
     radius: 0.5,
     material: &mat_dia1,
   });
   scene.spheres.push(Sphere {
-    center: glm::vec3(-1., 0., -1.),
+    center: Vec3f::new(-1., 0., -1.),
     radius: -0.45,
     material: &mat_dia1,
   });
 
   let cam = Camera {
-    lower_left_corner: glm::vec3(-2., -1., -1.),
-    horizontal: glm::vec3(4., 0., 0.),
-    vertical: glm::vec3(0., 2., 0.),
-    origin: glm::vec3(0., 0., 0.),
+    lower_left_corner: Vec3f::new(-2., -1., -1.),
+    horizontal: Vec3f::new(4., 0., 0.),
+    vertical: Vec3f::new(0., 2., 0.),
+    origin: Vec3f::new(0., 0., 0.),
   };
 
-  let rd = Uniform::from(0f32..1f32);
-
   const BYTES_PER_PIXEL: usize = 3;
-  let mut pixels = vec![0u8; dim.y as usize * dim.x as usize * BYTES_PER_PIXEL];
+  let mut pixels = vec![0u8; NY * NX * BYTES_PER_PIXEL];
   pixels
     .par_chunks_mut(BYTES_PER_PIXEL)
     .enumerate()
     .for_each(|(idx, chunk)| {
-      let xy = glm::vec2((idx % dim.x) as f32, (dim.y - 1 - idx / dim.x) as f32);
-      let mut c = glm::Vec3::zeros();
-      for _ in 0..ns {
-        let noise = RNG.with(|rng_rc| glm::Vec2::from_distribution(&rd, &mut *rng_rc.borrow_mut()));
-        let uv = (xy + noise).component_div(&glm::vec2(dim.x as f32, dim.y as f32));
-        let r = cam.gen_ray(uv);
+      let x = (idx % NX) as f32;
+      let y = (NY - 1 - idx / NX) as f32;
+      let mut c = Vec3f::zero();
+      for _ in 0..NS {
+        let (rx, ry) = RNG.with(|rng_rc| {
+          let mut rng = rng_rc.borrow_mut();
+          (rng.gen::<f32>(), rng.gen::<f32>())
+        });
+        let r = cam.gen_ray((x + rx) / NX as f32, (y + ry) / NY as f32);
         c += trace(&r, &scene, 0);
       }
       // The book uses a simple gamma 2.0 function, not the sRGB OETF.
-      c.apply(|e| (e / (ns as f32)).sqrt() * 255.99);
+      c.apply(|e| (e / (NS as f32)).sqrt() * 255.99);
       chunk[0] = c.x as u8;
       chunk[1] = c.y as u8;
       chunk[2] = c.z as u8;
     });
 
-  image::save_buffer(
-    "o.ppm",
-    &pixels[..],
-    dim.x as u32,
-    dim.y as u32,
-    image::RGB(8),
-  )
-  .unwrap()
+  image::save_buffer("o.ppm", &pixels[..], NX as u32, NY as u32, image::RGB(8)).unwrap()
 }
