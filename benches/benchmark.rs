47706be0734a733a3976e7ce997075974ea8d380
Jean-Francois Roy <jf@devklog.net>
2021-02-21 17:55:23 -0800

    Implement custom thread-local RNG and split scene gen from trace
    This patch implements a custom thread-local RNG using Xoshiro128Plus.
    This is the same RNG as rand::ThreadRNG, but it does not automatically
    reseed and it allows explicit control over the initial seed, which is
    important to have reproducible outputs for benchmarking.
    
    The patch also splits scene generation and scene tracing to exclude
    scene generation from benchmarking.
    
    AMD Ryzen 9 3900X 12-Core Processor (AMD64 Family 23 Model 113 Stepping 0)
    
    tracescene/10x10x4      time:   [193.07 us 193.55 us 194.06 us]
                            change: [-38.319% -38.103% -37.880%] (p = 0.00 < 0.05)
                            Performance has improved.
    Found 5 outliers among 100 measurements (5.00%)
      1 (1.00%) low mild
      2 (2.00%) high mild
      2 (2.00%) high severe

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,18 +1,24 @@
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
-use rand::prelude::*;
-use rrt::tracescene;
+use rand::SeedableRng;
+use rrt::{chap12_scene, init_pool_with_rng, tracescene, RttRng};
 use std::time::Duration;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
+  const NX: usize = 10;
+  const NY: usize = 10;
+  const NS: usize = 4;
+  let mut rng = RttRng::seed_from_u64(0);
+  let (scene, camera) = chap12_scene(NX, NY, &mut rng);
+  let pool = init_pool_with_rng(rng);
   c.bench_function("tracescene/10x10x4", move |b| {
-    const NX: usize = 10;
-    const NY: usize = 10;
-    const NS: usize = 4;
-    let mut rng = rand::rngs::SmallRng::seed_from_u64(0xC0DE_D09E);
     // tracescene returns a large Vec (the image), so use iter_batched to deal with the
     // memory drop. iter_with_large_drop is not suitable because the outputs are too large
     // to accumulate.
-    b.iter_batched(|| (), |_| tracescene(NX, NY, NS, &mut rng), BatchSize::SmallInput);
+    b.iter_batched(
+      || (),
+      |_| tracescene(NX, NY, NS, &scene, &camera, &pool),
+      BatchSize::SmallInput,
+    );
   });
 }
 
